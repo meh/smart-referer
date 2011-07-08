ba140b9ba1e4e63a9c6b99a16585bd1089a5ab0f
meh <meh@paranoici.org>
2011-07-08 13:59:22 +0200

    Fix version and license

--- components/do.js
+++ components/do.js
@@ -2,7 +2,7 @@
  *           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  *                   Version 2, December 2004
  *
- *  Copyleft meh.
+ *  Copyleft meh. [http://meh.paranoid.pk | meh@paranoici.org]
  *
  *           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
