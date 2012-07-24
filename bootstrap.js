58dd6fc868ac33a5c1cb5851fb020fa8bcbdc7cb
meh <meh@paranoici.org>
2012-07-24 19:34:24 +0200

    Improve code styling.

--- bootstrap.js
+++ bootstrap.js
@@ -13,12 +13,12 @@ const { classes: Cc, interfaces: Ci, results: Cr, utils: Cu } = Components;
 var Spoofer = (function () {
 	var c = function () {};
 
-	var Observer              = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
-	var NetworkIO             = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
-	var ScriptSecurityManager = Cc["@mozilla.org/scriptsecuritymanager;1"].getService(Ci.nsIScriptSecurityManager);
-	var EffectiveTLDService   = Cc["@mozilla.org/network/effective-tld-service;1"].getService(Ci.nsIEffectiveTLDService);
-	var Preferences           = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getBranch("extensions.smart-referer.");
-	var DefaultPreferences    = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getDefaultBranch("extensions.smart-referer.");
+	var Observer              = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService),
+	    NetworkIO             = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService),
+	    ScriptSecurityManager = Cc["@mozilla.org/scriptsecuritymanager;1"].getService(Ci.nsIScriptSecurityManager),
+	    EffectiveTLDService   = Cc["@mozilla.org/network/effective-tld-service;1"].getService(Ci.nsIEffectiveTLDService),
+	    Preferences           = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getBranch("extensions.smart-referer."),
+	    DefaultPreferences    = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getDefaultBranch("extensions.smart-referer.");
 
 	DefaultPreferences.setBoolPref("strict", true);
 	DefaultPreferences.setCharPref("mode", "direct");
@@ -42,102 +42,98 @@ var Spoofer = (function () {
 		return false;
 	}
 
-	function modify (http) {
-		try {
-			http.QueryInterface(Ci.nsIChannel);
+	c.prototype.observe = function (subject, topic, data) {
+		if (topic == "http-on-modify-request") {
+			var http    = subject.QueryInterface(Ci.nsIHttpChannel),
+			    referer = http.getRequestHeader("Referer");
 
-			var referer = NetworkIO.newURI(http.getRequestHeader("Referer"), null, null);
-		}
-		catch (e) {
-			return false;
-		}
-
-		try {
-			var toURI   = http.URI.clone(),
-			    fromURI = referer.clone();
-
-			if (fromURI.host == toURI.host || can("send", fromURI.host) || can("receive", toURI.host)) {
+			if (!referer) {
 				return false;
 			}
 
+			referer = NetworkIO.newURI(referer, null, null);
+
 			try {
-				var isIP = false;
+				var toURI   = http.URI.clone(),
+						fromURI = referer.clone();
 
-				EffectiveTLDService.getPublicSuffix(fromURI);
-				EffectiveTLDService.getPublicSuffix(toURI);
-			}
-			catch (e) {
-				if (e == Cr.NS_ERROR_HOST_IS_IP_ADDRESS) {
-					isIP = true;
-				}
-			}
-
-			if (!isIP) {
-				if (!Preferences.getBoolPref("strict")) {
-					let [from, to] = [fromURI, toURI].map(function (x) x.host.split('.').reverse());
-					let index      = 0;
-
-					while (from[index] || to[index]) {
-						if (from[index] == to[index]) {
-							index++;
-						}
-						else {
-							from.splice(index);
-							to.splice(index);
-						}
-					}
-
-					if (from.length == 0) {
-						throw Cr.NS_ERROR_DOM_BAD_URI;
-					}
-
-					fromURI.host = from.reverse().join('.');
-					toURI.host   = to.reverse().join('.');
+				if (fromURI.host == toURI.host || can("send", fromURI.host) || can("receive", toURI.host)) {
+					return false;
 				}
 
 				try {
-					if (EffectiveTLDService.getPublicSuffix(fromURI) == fromURI.host) {
-						throw Cr.NS_ERROR_DOM_BAD_URI;
-					}
+					var isIP = false;
+
+					EffectiveTLDService.getPublicSuffix(fromURI);
+					EffectiveTLDService.getPublicSuffix(toURI);
 				}
 				catch (e) {
-					if (e == Cr.NS_ERROR_DOM_BAD_URI) {
-						throw e;
+					if (e == Cr.NS_ERROR_HOST_IS_IP_ADDRESS) {
+						isIP = true;
 					}
 				}
-			}
 
-			ScriptSecurityManager.checkSameOriginURI(fromURI, toURI, false);
+				if (!isIP) {
+					if (!Preferences.getBoolPref("strict")) {
+						let [from, to] = [fromURI, toURI].map(function (x) x.host.split('.').reverse());
+						let index      = 0;
 
-			return false;
-		}
-		catch (e) {
-			var mode = Preferences.getCharPref("mode").trim();
+						while (from[index] || to[index]) {
+							if (from[index] == to[index]) {
+								index++;
+							}
+							else {
+								from.splice(index);
+								to.splice(index);
+							}
+						}
 
-			if (mode == "direct") {
-				referer = null;
-			}
-			else if (mode == "self") {
-				referer = http.URI;
-			}
-			else {
-				referer = Preferences.getCharPref("referer");
-			}
+						if (from.length == 0) {
+							throw Cr.NS_ERROR_DOM_BAD_URI;
+						}
 
-			if (typeof(referer) === "string") {
-				http.setRequestHeader("Referer", referer, false);
-			}
-			else {
-				http.referrer = referer;
-			}
+						fromURI.host = from.reverse().join('.');
+						toURI.host   = to.reverse().join('.');
+					}
 
-			return true;
-		}
-	}
+					try {
+						if (EffectiveTLDService.getPublicSuffix(fromURI) == fromURI.host) {
+							throw Cr.NS_ERROR_DOM_BAD_URI;
+						}
+					}
+					catch (e) {
+						if (e == Cr.NS_ERROR_DOM_BAD_URI) {
+							throw e;
+						}
+					}
+				}
 
-	c.prototype.observe = function (subject, topic, data) {
-		if (topic == "http-on-modify-request") {
-			modify(subject.QueryInterface(Ci.nsIHttpChannel));
+				ScriptSecurityManager.checkSameOriginURI(fromURI, toURI, false);
+
+				return false;
+			}
+			catch (e) {
+				var mode = Preferences.getCharPref("mode").trim();
+
+				if (mode == "direct") {
+					referer = null;
+				}
+				else if (mode == "self") {
+					referer = http.URI;
+				}
+				else {
+					referer = Preferences.getCharPref("referer");
+				}
+
+				if (typeof(referer) === "string") {
+					http.setRequestHeader("Referer", referer, false);
+				}
+				else {
+					http.referrer = referer;
+				}
+
+				return true;
+			}
 		}
 	}
 
