798a4083ffcd3e21377c0225ab9c88de7fb907cd
Alexander Schlarb <alexander@ninetailed.ninja>
2018-05-06 18:13:48 +0200

    Add new icon by @scribblemaniac with colors and 16x16 versions by me

--- background/icon.js
+++ background/icon.js
@@ -3,22 +3,53 @@ function generateIconTitle(enabled) {
 	return browser.runtime.getManifest().name + " – " + browser.i18n.getMessage(titleMsgID);
 }
 
+// Sniff for browsers supporting resetting the browserAction icon instead of just overriding it
+let browserSupportsIconReset = false;
+if(typeof(browser.browserAction.setIcon)  === "function"
+&& typeof(browser.runtime.getBrowserInfo) === "function") {
+	browser.runtime.getBrowserInfo().then(function(result) {
+		if(result.name === "Firefox" && parseInt(result.version.split(".")[0]) >= 59) {
+			browserSupportsIconReset = true;
+		}
+	});
+}
 
 // Monitor settings for changes to the request processing setting
 browser.storage.onChanged.addListener((changes, areaName) => {
 	for(let name of Object.keys(changes)) {
 		if(areaName === "local" && name === "enable") {
 			if(changes[name].newValue === true) {
+				if(browserSupportsIconReset) {
+					// Prefer to reset the browserAction icon instead of overring it so that the
+					// theme-specific theme_icons declared in the manifest can be used
+					browser.browserAction.setIcon({
+						path: null
+					});
 				//COMPAT: Firefox for Android 56+
-				if(typeof(browser.browserAction.setIcon) !== "undefined") {
-					browser.browserAction.setIcon({ path: { 256: "assets/icon.svg" } });
+				} else if(typeof(browser.browserAction.setIcon) !== "undefined") {
+					
+					browser.browserAction.setIcon({
+						path: {
+							// Use hand-rastorized icon for 16x16
+							16:  "assets/icon-enabled.16x16.png",
+							// SVG-scaling for well-enough for larger sizes
+							256: "assets/icon-enabled.svg"
+						}
+					});
 				}
 				
 				browser.browserAction.setTitle({ title: generateIconTitle(true) });
 			} else {
 				//COMPAT: Firefox for Android 56+
 				if(typeof(browser.browserAction.setIcon) !== "undefined") {
-					browser.browserAction.setIcon({ path: { 256: "assets/icon-light.svg" } });
+					browser.browserAction.setIcon({
+						path: {
+							// Use hand-rastorized icon for 16x16
+							16:  "assets/icon-disabled.16x16.png",
+							// SVG-scaling for well-enough for larger sizes
+							256: "assets/icon-disabled.svg"
+						}
+					});
 				}
 				
 				browser.browserAction.setTitle({ title: generateIconTitle(false) });
